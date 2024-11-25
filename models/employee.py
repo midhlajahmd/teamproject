f1f1dd0aad3b0ab04b454b478b41acf1702fe02e
midhlaj <midhlajahmdtk@gmail.com>
2024-11-25 17:12:28 +0530

    created employee class

--- models/employee.py
+++ models/employee.py
@@ -1,6 +1,13 @@
 class Employee:
-    def __init__(self, employeename, age):
-        self.employeeName = employeename
+    def __init__(self, name, age, doj, createdOn, isActive):
+        self.name = name
         self.age = age
+        self.doj = doj
+        self.createdOn = createdOn
+        self.isActive = isActive
     def display(self):
-        print(f"Employee Name is: {self.employeename}")
+        print(f"Employee Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Date of Joining: {self.doj}")
+        print(f"Created On: {self.createdOn}")
+        print(f"Active or Inactive: {self.isActive}")
