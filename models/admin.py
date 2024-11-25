e21349533051518b1c23346609a1358e11099c00
midhlaj <midhlajahmdtk@gmail.com>
2024-11-25 21:13:10 +0530

    deleted admin

--- models/admin.py
+++
@@ -1,6 +0,0 @@
-class admin:
-    def __init__(self, adminName, age):
-        self.adminName = adminName
-        self.age = age
-    def display(self):
-        print(f"Admin Name is: {self.adminName}")
