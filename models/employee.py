219fa283dcec4b642c76bd0046c8d853356d28e0
midhlaj <midhlajahmdtk@gmail.com>
2024-11-26 10:08:21 +0530

    updated employee class

--- models/employee.py
+++ models/employee.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 class Employee:
-    def __init__(self, name, age, doj, createdOn, isActive):
+    def __init__(self, name, age, doj, createdOn, isActive=True):
         self.name = name
         self.age = age
         self.doj = doj
-        self.createdOn = createdOn
+        self.createdOn = datetime.now().strftime("%d-%m-%Y")
         self.isActive = isActive
     def display(self):
+        print("--- Employee Details ---")
         print(f"Employee Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Date of Joining: {self.doj}")
